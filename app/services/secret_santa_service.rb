e26f555fb63c78edbf8f75bb7753e059aa32be51
Ryan Workman <rdavid1099@users.noreply.github.com>
2020-12-06 13:39:47 -0800

    Add secret santa message delivery

--- app/services/secret_santa_service.rb
+++ app/services/secret_santa_service.rb
@@ -1,12 +1,32 @@
 class SecretSantaService
-  attr_reader :user
+  SECRET_SANTA_BOT_ID = "U01GEG9T46L"
+
+  attr_reader :user, :direct_message_response
 
   def initialize(user)
     @user = user
+    @direct_message_response
+  end
+
+  def handle_message(raw_text:, mentioned_ids:)
+    unless mentioned_ids.one?
+      set_unknown_recipient_response(mentioned_ids.count)
+      return
+    end
+
+    recipient = User.find_by(slack_id: mentioned_ids.first)
+    message = raw_text.sub(/<([^>]*)>/, "").strip
+    response = send_message(to: recipient, text: message)
+
+    if response[:ok]
+      set_successful_response(recipient)
+    else
+      set_failed_response(recipient)
+    end
   end
 
   def send_introduction
-    initiate_conversation
+    initiate_conversation(user)
 
     return false if user.dm_channel_id.blank?
 
@@ -19,26 +39,41 @@ class SecretSantaService
     responses.all?(true)
   end
 
-  private
+  def send_message(to:, text:)
+    initiate_conversation(to)
 
-  def initiate_conversation
-    return if user.dm_channel_id.present?
+    return false if to.dm_channel_id.blank?
 
-    dm_channel_id = get_dm_channel_id
-
-    user.update(dm_channel_id: dm_channel_id)
+    post_message(
+      channel: to.dm_channel_id,
+      text: "#{dm_gift_recipient_intro}>#{text}\n\n#{dm_response_info}"
+    )
   end
 
-  def get_dm_channel_id
+  private
+
+  def initiate_conversation(init_user)
+    return if init_user.dm_channel_id.present?
+
+    dm_channel_id = get_dm_channel_id(init_user.slack_id)
+
+    init_user.update(dm_channel_id: dm_channel_id)
+  end
+
+  def get_dm_channel_id(slack_id)
     response = slack_msgr.conversations(
       :open,
       token: slack_msgr.configuration.access_tokens[:bot],
-      users: user.slack_id,
+      users: slack_id,
     )
 
     return unless response[:ok]
 
     response[:channel][:id]
+  end
+
+  def post_message(**opts)
+    slack_msgr.chat(:post_message, opts)
   end
 
   def send_dm(text)
@@ -47,6 +82,22 @@ class SecretSantaService
 
   def slack_msgr
     Rails.configuration.slack_msgr
+  end
+
+  def set_successful_response(recipient)
+    @direct_message_response = "Your message is on the way to <@#{recipient.slack_id}>! I'll notify you when they respond."
+  end
+
+  def set_failed_response(recipient)
+    @direct_message_response = "Ugh... I think I've run out of magic. I couldn't deliver your message to <@#{recipient.slack_id}>. Please try again later. :disappointed:"
+  end
+
+  def set_unknown_recipient_response(count)
+    @direct_message_response = if count.zero?
+      "Who do you want me to send this message to? Don't forget to include <@#{user.recipient.slack_id}> to send a message to your gift recipient."
+    else
+      "Whoa! Pull my beard and call me an elf, I can't send that many messages. Please only have one person included in your message below to send your message. For example: \"<@#{user.recipient.slack_id}> What would you like for Christmas?\""
+    end
   end
 
   def introduction_snippets
@@ -61,4 +112,11 @@ class SecretSantaService
     ]
   end
 
+  def dm_gift_recipient_intro
+    "Ho ho ho! Merry Christmas and season's greetings. My Secret Santa helper has a message for you below.\n\n"
+  end
+
+  def dm_response_info
+    "To repond, simply begin your message with '<@#{SECRET_SANTA_BOT_ID}>', and I'll send your response to your Seret Santa."
+  end
 end
